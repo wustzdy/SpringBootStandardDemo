56bda9151d609ca66028c2d1d3bc6964b7c87434
wustzdy <714403855@qq.com>
2020-07-27 22:03:36 +0800

    init project

---
+++ src/main/java/com/example/springboot/standard/demo/SpringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.standard.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboottestApplication.class, args);
+	}
+
+}
